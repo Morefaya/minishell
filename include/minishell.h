d91ff3f43499cec3199d53256c51997667813e23
Jean-charles CAZAKO <jcazako@e1r9p8.42.fr>
2016-07-04 16:50:45 +0200

    starting norme

--- include/minishell.h
+++ include/minishell.h
@@ -6,7 +6,7 @@
 /*   By: jcazako <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/06/11 16:31:43 by jcazako           #+#    #+#             */
-/*   Updated: 2016/06/20 14:22:04 by jcazako          ###   ########.fr       */
+/*   Updated: 2016/07/04 16:44:51 by jcazako          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,6 @@
 # include "libft.h"
 # include <signal.h>
 # include <unistd.h>
-
-
 
 typedef struct	s_shell
 {
@@ -30,36 +28,36 @@ void			del_content(t_shell *content, size_t size);
 t_list			*read_cmd(void);
 void			print_tab2d(char **tab);
 void			free_tab2d(char **tab);
-int			builtins(t_list *lst, t_list **env_l);
+int				builtins(t_list *lst, t_list **env_l);
 char			**get_paths_vars(t_list *env_l);
 char			*make_cmd_path(char *path, char *str);
 char			**get_arg(char *str);
 char			*get_cmd(char *str);
 char			**tab2d_lst(t_list *lst);
-int			minishell(t_list *cmd_l, t_list **env_l);
+int				minishell(t_list *cmd_l, t_list **env_l);
 t_list			*lstenv_cpy(t_list *env);
 void			putillegal_opt_env(char a);
 void			delete_env(char **m, t_list **env_c);
-int			ft_unsetenv(t_list *cmd_l, t_list **env_l);
-int			check_arg(char *str);
+int				ft_unsetenv(t_list *cmd_l, t_list **env_l);
+int				check_arg(char *str);
 void			del_linkenv(char *u_env, t_list **env_l);
-int			ft_setenv(t_list *cmd_l, t_list **env_l);
-int			ft_env(t_list *cmd_l, t_list *env_l);
+int				ft_setenv(t_list *cmd_l, t_list **env_l);
+int				ft_env(t_list *cmd_l, t_list *env_l);
 void			unset_ft_env(char **str, t_list **env_c);
 void			set_env(char *str, t_list **env_l);
 void			freed(t_list **to_free);
 void			freed_all(t_list **begin_free);
 void			unsetenv_format(void);
 void			exe_cmd(char *cmd, t_list *env_l);
-int			ft_exit(void);
-int			ft_cd(t_list *lst, t_list **env_l);
-int			tablen(char **tab);
+int				ft_exit(void);
+int				ft_cd(t_list *lst, t_list **env_l);
+int				tablen(char **tab);
 char			*get_a_pwd(void);
 char			*get_pwd(void);
 char			*get_var_env(char *env_var, t_list *env_l);
 char			*chdir_cd(char *str);
-int			cd_set(char *owd, char *awd, char *cmd, t_list **env_l);
-int			cmp_env(char *str1, char *str2);
+int				cd_set(char *owd, char *awd, char *cmd, t_list **env_l);
+int				cmp_env(char *str1, char *str2);
 char			*gt_env_var(char *str);
 t_list			*checkout(char **arg, int *i, t_list *env_c, int *illegal);
 char			*check_u(char *str);
@@ -67,6 +65,6 @@ char			*trim_name(char *str);
 void			set_it(char *arg, t_list **env_c);
 char			*execute_2(char **cmd);
 void			execute(char **cmd, t_list **env_c);
-int			unset_it(t_list *unset, t_list **env_c);
-int			ft_echo(t_list *lst, t_list **env_l);
+int				unset_it(t_list *unset, t_list **env_c);
+int				ft_echo(t_list *lst, t_list **env_l);
 #endif
--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: jcazako <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/05/30 20:38:42 by jcazako           #+#    #+#             */
-/*   Updated: 2016/06/14 17:25:00 by jcazako          ###   ########.fr       */
+/*   Updated: 2016/07/04 16:45:27 by jcazako          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,15 +16,12 @@ int	main(int ac, char **av, char **env)
 {
 	t_list	*env_l;
 	t_list	*cmd_l;
-	//char	**path_t;
 
 	ac = 0;
 	av++;
 	cmd_l = NULL;
 	if (!(env_l = lst_tab2d(env)))
 		ft_putstr("ENV_NULL");
-	//if (!(path_t = get_paths_vars(env_l)))
-	//	ft_putendl("PATH_NULL");
 	signal(SIGINT, SIG_IGN);
 	while (42)
 	{
@@ -33,7 +30,6 @@ int	main(int ac, char **av, char **env)
 		minishell(cmd_l, &env_l);
 		ft_lstdel(&cmd_l, (void(*)(void*, size_t))del_content);
 	}
-//	free_tab2d(path_t);
 	ft_lstdel(&env_l, (void(*)(void*, size_t))del_content);
 	return (0);
 }
